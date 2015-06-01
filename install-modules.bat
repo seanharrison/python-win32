2e6a751296160f56b5be8ebab5a4300994cc814a
Sean Harrison <sah@blackearth.us>
2015-06-01 14:55:16 -0500

    install-modules now contains what we need to install python modules

---
+++ install-modules.bat
@@ -0,0 +1,2 @@
+pip install -q "%~dp0\lxml-3.4.4-cp34-none-win32.whl"
+pip install -q "%~dp0\Unum-4.1.3-py2.py3-none-any.whl"
--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 "%~dp0\python-3.4.3.msi"
 "%~dp0\pywin32-219.win32-py3.4.exe"
-pip install -q "%~dp0\lxml-3.4.4-cp34-none-win32.whl"
-pip install -q "%~dp0\Unum-4.1.3-py2.py3-none-any.whl"
+"%~dp0\install-modules.bat"
+
